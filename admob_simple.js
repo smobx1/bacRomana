3764045ddb3d4677633349939297986a14e6c176
smobx1 <mnmoldoveanu@gmail.com>
2018-11-01 19:25:41 +0400

    test_admob_nou
    test_ad

--- admob_simple.js
+++ admob_simple.js
@@ -1,11 +1,24 @@
-admob.initAdmob("ca-app-pub-2789022727093400/9133177509");
+alert("inainte");
 
-document.addEventListener(admob.Event.onInterstitialReceive, this.onInterstitialReceive, false);
-admob.cacheInterstitial();
-
-
-admob.isInterstitialReady(function(isReady){
-    if(isReady){
-        admob.showInterstitial();
+function onDeviceReady() {
+      document.removeEventListener('deviceready', onDeviceReady, false);
+      
+      // Set AdMobAds options:
+      admob.setOptions({
+        publisherId:          "pub-2789022727093400",  // Required
+        interstitialAdId:     "ca-app-pub-2789022727093400/9133177509",  // Optional
+        tappxIdiOS:           "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
+        tappxIdAndroid:       "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
+        tappxShare:           0.5                                        // Optional
+      });
+      
+      // Start showing banners (atomatic when autoShowBanner is set to true)
+      admob.createBannerView();
+      
+      // Request interstitial (will present automatically when autoShowInterstitial is set to true)
+      admob.requestInterstitialAd();
     }
-});
+    
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+alert("dupa");
