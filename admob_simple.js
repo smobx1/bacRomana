31ad5632142ab37efccfb5cbe4286766cea10d30
smobx1 <mnmoldoveanu@gmail.com>
2018-11-17 20:51:49 +0000

    test
    t

--- admob_simple.js
+++ admob_simple.js
@@ -1,89 +1,89 @@
-// // alert("inainte");
+// alert("inainte");
 
-//  var isAppForeground = true;
+ var isAppForeground = true;
     
-//     function initAds() {
-//       if (admob) {
-//         var adPublisherIds = {
-//           ios : {
-//             // banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
-//             interstitial : "ca-app-pub-2789022727093400/9133177509"
-//           },
-//           android : {
-//             // banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
-//             interstitial : "ca-app-pub-2789022727093400/5800437153"
-//           }
-//         };
+    function initAds() {
+      if (admob) {
+        var adPublisherIds = {
+          ios : {
+            // banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
+            interstitial : "ca-app-pub-2789022727093400/9133177509"
+          },
+          android : {
+            // banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
+            interstitial : "ca-app-pub-2789022727093400/5800437153"
+          }
+        };
         
-//         var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
             
-//         admob.setOptions({
-//           publisherId:      admobid.banner,
-//           interstitialAdId: admobid.interstitial,
-//           tappxIdiOS:       "/XXXXXXXXX/Pub-XXXX-iOS-IIII",
-//           tappxIdAndroid:   "/XXXXXXXXX/Pub-XXXX-Android-AAAA",
-//           tappxShare:       0.5,
-//           isTesting: true, // receiving test ads (do not test with real ads as your account will be banned)
+        admob.setOptions({
+          publisherId:      admobid.banner,
+          interstitialAdId: admobid.interstitial,
+          tappxIdiOS:       "/XXXXXXXXX/Pub-XXXX-iOS-IIII",
+          tappxIdAndroid:   "/XXXXXXXXX/Pub-XXXX-Android-AAAA",
+          tappxShare:       0.5,
+          isTesting: true, // receiving test ads (do not test with real ads as your account will be banned)
           
-//         });
+        });
 
-//         registerAdEvents();
+        registerAdEvents();
         
-//       } else {
-//         alert('AdMobAds plugin not ready');
-//       }
-//     }
+      } else {
+        alert('AdMobAds plugin not ready');
+      }
+    }
     
-//     function onAdLoaded(e) {
-//       if (isAppForeground) {
-//         if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
-//           console.log("An interstitial has been loaded and autoshown. If you want to load the interstitial first and show it later, set 'autoShowInterstitial: false' in admob.setOptions() and call 'admob.showInterstitialAd();' here");
-//         } else if (e.adType === admob.AD_TYPE_BANNER) {
-//           console.log("New banner received");
-//         }
-//       }
-//     }
+    function onAdLoaded(e) {
+      if (isAppForeground) {
+        if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
+          console.log("An interstitial has been loaded and autoshown. If you want to load the interstitial first and show it later, set 'autoShowInterstitial: false' in admob.setOptions() and call 'admob.showInterstitialAd();' here");
+        } else if (e.adType === admob.AD_TYPE_BANNER) {
+          console.log("New banner received");
+        }
+      }
+    }
     
-//     function onPause() {
-//       if (isAppForeground) {
-//         admob.destroyBannerView();
-//         isAppForeground = false;
-//       }
-//     }
+    function onPause() {
+      if (isAppForeground) {
+        admob.destroyBannerView();
+        isAppForeground = false;
+      }
+    }
     
-//     function onResume() {
-//       if (!isAppForeground) {
-//         setTimeout(admob.createBannerView, 1);
-//         setTimeout(admob.requestInterstitialAd, 1);
-//         isAppForeground = true;
-//       }
-//     }
+    function onResume() {
+      if (!isAppForeground) {
+        setTimeout(admob.createBannerView, 1);
+        setTimeout(admob.requestInterstitialAd, 1);
+        isAppForeground = true;
+      }
+    }
     
-//     // optional, in case respond to events
-//     function registerAdEvents() {
-//       document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
-//       document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
-//       document.addEventListener(admob.events.onAdOpened, function (e) {});
-//       document.addEventListener(admob.events.onAdClosed, function (e) {});
-//       document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
+    // optional, in case respond to events
+    function registerAdEvents() {
+      document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
+      document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
+      document.addEventListener(admob.events.onAdOpened, function (e) {});
+      document.addEventListener(admob.events.onAdClosed, function (e) {});
+      document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
       
-//       document.addEventListener("pause", onPause, false);
-//       document.addEventListener("resume", onResume, false);
-//     }
+      document.addEventListener("pause", onPause, false);
+      document.addEventListener("resume", onResume, false);
+    }
         
-//     function onDeviceReady() {
-//       document.removeEventListener('deviceready', onDeviceReady, false);
-//       initAds();
+    function onDeviceReady() {
+      document.removeEventListener('deviceready', onDeviceReady, false);
+      initAds();
 
-//       // display a banner at startup
-//       // admob.createBannerView();
+      // display a banner at startup
+      // admob.createBannerView();
         
-//       // request an interstitial
-//       admob.requestInterstitialAd();
+      // request an interstitial
+      admob.requestInterstitialAd();
 
-//     }
+    }
     
-//     document.addEventListener("deviceready", onDeviceReady, false);
+    document.addEventListener("deviceready", onDeviceReady, false);
 
 
-// // alert("dupa");
+// alert("dupa");
