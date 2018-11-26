4ec0de3653c5662dc8334508d532250881ddfa20
smobx1 <mnmoldoveanu@gmail.com>
2018-11-26 02:28:28 +0000

    test
    t

--- admob.js
+++ admob.js
@@ -1,69 +1,69 @@
-		function initAds() {
-	      if (admob) {
-	        var adPublisherIds = {
-	          ios : {
-	            // banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
-	            interstitial : "ca-app-pub-2789022727093400/9133177509"
-	          },
-	          android : {
-	            // banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
-	            interstitial : "ca-app-pub-2789022727093400/5800437153"
-	          }
-	        };
+		// function initAds() {
+	 //      if (admob) {
+	 //        var adPublisherIds = {
+	 //          ios : {
+	 //            // banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
+	 //            interstitial : "ca-app-pub-2789022727093400/9133177509"
+	 //          },
+	 //          android : {
+	 //            // banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
+	 //            interstitial : "ca-app-pub-2789022727093400/5800437153"
+	 //          }
+	 //        };
 	    	  
-	        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+	 //        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
 	            
-	        admob.setOptions({
-	          publisherId: "pub-2789022727093400",
-	          interstitialAdId: admobid.interstitial,
-	          // isTesting: true, 
-	          // tappxIdiOS:       "/XXXXXXXXX/Pub-XXXX-iOS-IIII",
-	          // tappxIdAndroid:   "/XXXXXXXXX/Pub-XXXX-Android-AAAA",
-	          tappxShare:       0.5
-	        });
+	 //        admob.setOptions({
+	 //          publisherId: "pub-2789022727093400",
+	 //          interstitialAdId: admobid.interstitial,
+	 //          // isTesting: true, 
+	 //          // tappxIdiOS:       "/XXXXXXXXX/Pub-XXXX-iOS-IIII",
+	 //          // tappxIdAndroid:   "/XXXXXXXXX/Pub-XXXX-Android-AAAA",
+	 //          tappxShare:       0.5
+	 //        });
 
-	        registerAdEvents();
+	 //        registerAdEvents();
 	        
-	      } else {
-	        alert('AdMobAds plugin not ready');
-	      }
-	    }
+	 //      } else {
+	 //        alert('AdMobAds plugin not ready');
+	 //      }
+	 //    }
     
-    function onAdLoaded(e) {
-      // if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
-      //   admob.showInterstitialAd();
-      //   showNextInterstitial = setTimeout(function() {
-      //     admob.requestInterstitialAd();
-      //   }, 2 * 60 * 1000); // 2 minutes
-      // }
-    }
+  //   function onAdLoaded(e) {
+  //     // if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
+  //     //   admob.showInterstitialAd();
+  //     //   showNextInterstitial = setTimeout(function() {
+  //     //     admob.requestInterstitialAd();
+  //     //   }, 2 * 60 * 1000); // 2 minutes
+  //     // }
+  //   }
     
-    // optional, in case respond to events
-    function registerAdEvents() {
-      document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
-      document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
-      document.addEventListener(admob.events.onAdOpened, function (e) {});
-      document.addEventListener(admob.events.onAdClosed, function (e) {});
-      document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
-      document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) {});
-    }
+  //   // optional, in case respond to events
+  //   function registerAdEvents() {
+  //     document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
+  //     document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
+  //     document.addEventListener(admob.events.onAdOpened, function (e) {});
+  //     document.addEventListener(admob.events.onAdClosed, function (e) {});
+  //     document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
+  //     document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) {});
+  //   }
         
-    function onDeviceReady() {
-      document.removeEventListener('deviceready', onDeviceReady, false);
-      initAds();
+  //   function onDeviceReady() {
+  //     document.removeEventListener('deviceready', onDeviceReady, false);
+  //     initAds();
 
-      // display a banner at startup
-      // admob.createBannerView();
+  //     // display a banner at startup
+  //     // admob.createBannerView();
         
-        // request an interstitial
+  //       // request an interstitial
         
-        // aman 1 secunda reclama
-	    setTimeout(doSomething, 750);
+  //       // aman 1 secunda reclama
+	 //    setTimeout(doSomething, 750);
 
-		function doSomething() {
-		   //do whatever you want here
-		   admob.requestInterstitialAd();
-		}
-    }
+		// function doSomething() {
+		//    //do whatever you want here
+		//    admob.requestInterstitialAd();
+		// }
+  //   }
     
-    document.addEventListener("deviceready", onDeviceReady, false);
+  //   document.addEventListener("deviceready", onDeviceReady, false);
