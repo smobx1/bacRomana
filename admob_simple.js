6f418db20777bfe7e843100d4a2af902405e9a43
smobx1 <mnmoldoveanu@gmail.com>
2018-12-03 19:02:13 +0000

    teste

---
+++ admob_simple.js
@@ -0,0 +1,22 @@
+var admobid = {};
+// select the right Ad Id according to platform
+if( /(android)/i.test(navigator.userAgent) ) { 
+    admobid = { // for Android
+        banner: 'ca-app-pub-6869992474017983/9375997553',
+        interstitial: 'ca-app-pub-6869992474017983/1657046752'
+    };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+    admobid = { // for iOS
+        banner: 'ca-app-pub-6869992474017983/4806197152',
+        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+    };
+} else {
+    admobid = { // for Windows Phone
+        banner: 'ca-app-pub-6869992474017983/8878394753',
+        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+    };
+}
+
+
+// it will display smart banner at top center, using the default options
+if(AdMob) AdMob.createBanner( admobid.banner );
