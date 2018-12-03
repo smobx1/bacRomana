6b239482aaf96054f3df2a5e25d286d14d4a7e7c
smobx1 <mnmoldoveanu@gmail.com>
2018-12-03 17:01:40 +0000

    banner+inter

--- admob.js
+++ admob.js
@@ -1,7 +1,42 @@
-function onDeviceReady() {
-      admob.createBannerView({ publisherId: 'pub-2789022727093400' });
-}
+    function initAds() {
+      if (admob) {
+        var adPublisherIds = {
+          ios : {
+            banner : "ca-app-pub-2789022727093400/1079470197",
+            interstitial : "ca-app-pub-2789022727093400/9133177509"
+          },
+          android : {
+            banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
+            interstitial : "ca-app-pub-XXXXXXXXXXXXXXXX/IIIIIIIIII"
+          }
+        };
+    	  
+        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+            
+        admob.setOptions({
+          publisherId: "pub-2789022727093400"
+        });
 
-
+        registerAdEvents();
+        
+      } else {
+        alert('AdMobAds plugin not ready');
+      }
+    }
     
-document.addEventListener("deviceready", onDeviceReady, false);
+    
+    
+    
+        
+    function onDeviceReady() {
+      document.removeEventListener('deviceready', onDeviceReady, false);
+      initAds();
+
+      // display a banner at startup
+      admob.createBannerView();
+        
+      // request an interstitial
+      admob.requestInterstitialAd();
+    }
+    
+    document.addEventListener("deviceready", onDeviceReady, false);
