cd1688c54c60bb257ac81aa0fdd85f050aedaec1
smobx1 <mnmoldoveanu@gmail.com>
2018-12-03 17:05:04 +0000

    test

--- admob.js
+++ admob.js
@@ -1,4 +1,4 @@
-    function initAds() {
+function initAds() {
       if (admob) {
         var adPublisherIds = {
           ios : {
@@ -6,15 +6,19 @@
             interstitial : "ca-app-pub-2789022727093400/9133177509"
           },
           android : {
-            banner : "ca-app-pub-XXXXXXXXXXXXXXXX/BBBBBBBBBB",
-            interstitial : "ca-app-pub-XXXXXXXXXXXXXXXX/IIIIIIIIII"
+            banner : "ca-app-pub-2789022727093400/7457176289",
+            interstitial : "ca-app-pub-2789022727093400/5800437153"
           }
         };
     	  
         var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
             
         admob.setOptions({
-          publisherId: "pub-2789022727093400"
+          publisherId: "pub-2789022727093400",
+          interstitialAdId: admobid.interstitial,
+          tappxIdiOS:       "/XXXXXXXXX/Pub-XXXX-iOS-IIII",
+          tappxIdAndroid:   "/XXXXXXXXX/Pub-XXXX-Android-AAAA",
+          tappxShare:       0.5
         });
 
         registerAdEvents();
@@ -24,9 +28,24 @@
       }
     }
     
+    function onAdLoaded(e) {
+      if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
+        admob.showInterstitialAd();
+        showNextInterstitial = setTimeout(function() {
+          admob.requestInterstitialAd();
+        }, 2 * 60 * 1000); // 2 minutes
+      }
+    }
     
-    
-    
+    // optional, in case respond to events
+    function registerAdEvents() {
+      document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
+      document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
+      document.addEventListener(admob.events.onAdOpened, function (e) {});
+      document.addEventListener(admob.events.onAdClosed, function (e) {});
+      document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
+      document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) {});
+    }
         
     function onDeviceReady() {
       document.removeEventListener('deviceready', onDeviceReady, false);
