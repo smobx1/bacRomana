abc46d538738d1ca7068eef541a94cec9111c36c
smobx1 <mnmoldoveanu@gmail.com>
2018-12-03 19:07:16 +0000

    test

--- admob_simple.js
+++ admob_simple.js
@@ -1,22 +1,70 @@
-var admobid = {};
-// select the right Ad Id according to platform
-if( /(android)/i.test(navigator.userAgent) ) { 
-    admobid = { // for Android
-        banner: 'ca-app-pub-6869992474017983/9375997553',
-        interstitial: 'ca-app-pub-6869992474017983/1657046752'
-    };
-} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
-    admobid = { // for iOS
-        banner: 'ca-app-pub-6869992474017983/4806197152',
-        interstitial: 'ca-app-pub-6869992474017983/7563979554'
-    };
-} else {
-    admobid = { // for Windows Phone
-        banner: 'ca-app-pub-6869992474017983/8878394753',
-        interstitial: 'ca-app-pub-6869992474017983/1355127956'
-    };
-}
+		function initAds() {
+	      if (admob) {
+	        var adPublisherIds = {
+	          ios : {
+	            banner : "ca-app-pub-2789022727093400/1079470197",
+	            interstitial : "ca-app-pub-2789022727093400/9133177509"
+	          },
+	          android : {
+	            banner : "ca-app-pub-2789022727093400/7457176289"
+	            // interstitial : "ca-app-pub-2789022727093400/5800437153"
+	          }
+	        };
+	    	  
+	        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+	            
+	        admob.setOptions({
+	          publisherId: "pub-2789022727093400"
+	          // interstitialAdId: admobid.interstitial,
+	          // autoShowBanner:       true,
+	          // isTesting: true, 
+	          // tappxIdiOS:       "/XXXXXXXXX/Pub-XXXX-iOS-IIII",
+	          // tappxIdAndroid:   "/XXXXXXXXX/Pub-XXXX-Android-AAAA",
+	          // tappxShare:       0.5
+	        });
 
+	        registerAdEvents();
+	        
+	      } else {
+	        alert('AdMobAds plugin not ready');
+	      }
+	    }
+    
+    function onAdLoaded(e) {
+      // if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
+      //   admob.showInterstitialAd();
+      //   showNextInterstitial = setTimeout(function() {
+      //     admob.requestInterstitialAd();
+      //   }, 2 * 60 * 1000); // 2 minutes
+      // }
+    }
+    
+    // optional, in case respond to events
+    function registerAdEvents() {
+      document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
+      document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
+      document.addEventListener(admob.events.onAdOpened, function (e) {});
+      document.addEventListener(admob.events.onAdClosed, function (e) {});
+      document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
+      document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) {});
+    }
+        
+    function onDeviceReady() {
+      document.removeEventListener('deviceready', onDeviceReady, false);
+      initAds();
 
-// it will display smart banner at top center, using the default options
-if(AdMob) AdMob.createBanner( admobid.banner );
+      // display a banner at startup
+      admob.createBannerView();
+        
+        // request an interstitial
+        
+        // aman 1 secunda reclama
+	    // setTimeout(doSomething, 550);
+
+		// function doSomething() {
+		//    //do whatever you want here
+		//    admob.requestInterstitialAd();
+		// }
+    }
+    
+    document.addEventListener("deviceready", onDeviceReady, false);
