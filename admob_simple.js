9015e7b7ddba4c9fca29654c585236477ccc4103
smobx1 <mnmoldoveanu@gmail.com>
2018-12-03 19:48:53 +0000

    no ads

--- admob_simple.js
+++ admob_simple.js
@@ -1,63 +1,63 @@
-    function initAds() {
-      if (admob) {
-        var adPublisherIds = {
-          ios : {
-            banner : "ca-app-pub-2789022727093400/9527841826",
-            interstitial : "ca-app-pub-2789022727093400/9133177509"
-          },
-          android : {
-            banner : "ca-app-pub-2789022727093400/9145285178",
-            interstitial : "ca-app-pub-2789022727093400/5800437153"
-          }
-        };
+  //   function initAds() {
+  //     if (admob) {
+  //       var adPublisherIds = {
+  //         ios : {
+  //           banner : "ca-app-pub-2789022727093400/9527841826",
+  //           interstitial : "ca-app-pub-2789022727093400/9133177509"
+  //         },
+  //         android : {
+  //           banner : "ca-app-pub-2789022727093400/9145285178",
+  //           interstitial : "ca-app-pub-2789022727093400/5800437153"
+  //         }
+  //       };
     	  
-        var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+  //       var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
             
-        admob.setOptions({
-          publisherId:      admobid.banner,
-          interstitialAdId: admobid.interstitial
-          // isTesting: true
-        });
+  //       admob.setOptions({
+  //         publisherId:      admobid.banner,
+  //         interstitialAdId: admobid.interstitial
+  //         // isTesting: true
+  //       });
 
-        registerAdEvents();
+  //       registerAdEvents();
         
-      } else {
-        alert('AdMobAds plugin not ready');
-      }
-    }
+  //     } else {
+  //       alert('AdMobAds plugin not ready');
+  //     }
+  //   }
     
-    function onAdLoaded(e) {
-      if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
-        admob.showInterstitialAd();
-        showNextInterstitial = setTimeout(function() {
-          admob.requestInterstitialAd();
-        }, 2 * 60 * 1000); // 2 minutes
-      }
-    }
+  //   function onAdLoaded(e) {
+  //     if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
+  //       admob.showInterstitialAd();
+  //       showNextInterstitial = setTimeout(function() {
+  //         admob.requestInterstitialAd();
+  //       }, 2 * 60 * 1000); // 2 minutes
+  //     }
+  //   }
     
-    // optional, in case respond to events
-    function registerAdEvents() {
-      document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
-      document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
-      document.addEventListener(admob.events.onAdOpened, function (e) {});
-      document.addEventListener(admob.events.onAdClosed, function (e) {});
-      document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
-      document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) {});
-    }
+  //   // optional, in case respond to events
+  //   function registerAdEvents() {
+  //     document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
+  //     document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
+  //     document.addEventListener(admob.events.onAdOpened, function (e) {});
+  //     document.addEventListener(admob.events.onAdClosed, function (e) {});
+  //     document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
+  //     document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) {});
+  //   }
         
-    function onDeviceReady() {
-      document.removeEventListener('deviceready', onDeviceReady, false);
-      initAds();
+  //   function onDeviceReady() {
+  //     document.removeEventListener('deviceready', onDeviceReady, false);
+  //     initAds();
 
-      // display a banner at startup
-      admob.createBannerView();              
+  //     // display a banner at startup
+  //     admob.createBannerView();              
 
-      	setTimeout(doSomething, 10000);
+  //     	setTimeout(doSomething, 10000);
 
-		function doSomething() {
-		   //do whatever you want here
-		   admob.requestInterstitialAd();
-		}
-    }
+		// function doSomething() {
+		//    //do whatever you want here
+		//    admob.requestInterstitialAd();
+		// }
+  //   }
     
-    document.addEventListener("deviceready", onDeviceReady, false);
+  //   document.addEventListener("deviceready", onDeviceReady, false);
