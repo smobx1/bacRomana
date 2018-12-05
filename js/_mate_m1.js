1217bc6d45edc6e5e2ffd7370fa88158bd094941
smobx1 <mnmoldoveanu@gmail.com>
2018-12-05 19:51:34 +0000

    test

---
+++ js/_mate_m1.js
@@ -0,0 +1,276 @@
+////////////////////////////////////////////////// EPIC
+/*
+UMAN UMAN UMAN UMAN UMAN UMAN UMAN UMAN UMAN UMAN UMAN UMAN UMAN UMAN
+*/
+// mihnea SI IOANA
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+if(document.getElementById("title").innerHTML == "Lectie1"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+
+if(document.getElementById("title").innerHTML == "Lectie2"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+
+if(document.getElementById("title").innerHTML == "Lectie3"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+
+if(document.getElementById("title").innerHTML == "Lectie4"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+
+if(document.getElementById("title").innerHTML == "Lectie5"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie6"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie7"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie8"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie9"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie10"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie11"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie12"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie13"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie14"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie15"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie16"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie17"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie18"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie19"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+if(document.getElementById("title").innerHTML == "Lectie20"){
+  var continut = document.getElementById("continut");
+  continut.innerHTML += "<img src='../img/formule_m1/multimi/multimi_1.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_2.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>"+
+                        "<img src='../img/formule_m1/multimi/multimi_3.jpg' style='max-width:100%;'>";
+}
+
+
+
+$('<div class="app-nav-bottom" id="app-nav-bottom"><table class="app-nav-tabel"><tr><td><a href="../index.html"><i class="fa fa-home fa-nav fa-nav-size"></i></a></td><td><a href="../uman.html"><i class="fa fa-book fa-nav fa-nav-size"></i></a></td><td><a href="../uman.html"><i class="fa fa-reply fa-nav fa-nav-size"></i></a></td></tr></table></div>').insertAfter("#continut");
+
+
+
+
+
+
+
+// http://stackoverflow.com/a/9795091
+$.fn.wrapInTag = function (opts) {
+    // http://stackoverflow.com/a/1646618
+    function getText(obj) {
+        return obj.textContent ? obj.textContent : obj.innerText;
+    }
+
+    var tag = opts.tag || 'strong',
+        words = opts.words || [],
+        regex = RegExp(words.join('|'), 'gi'),
+        replacement = '<' + tag + '>$&</' + tag + '>';
+
+    // http://stackoverflow.com/a/298758
+    $(this).contents().each(function () {
+        if (this.nodeType === 3) //Node.TEXT_NODE
+        {
+            // http://stackoverflow.com/a/7698745
+            $(this).replaceWith(getText(this).replace(regex, replacement));
+        }
+        else if (!opts.ignoreChildNodes) {
+            $(this).wrapInTag(opts);
+        }
+    });
+};
+
+// highlight these words
+$('p').wrapInTag({"words" : [
+            "arta poetica ",
+            "axul ",
+            "conflictul ",
+            "cadrul ",
+            "conflictul interior ",
+            "conflictul exterior ",
+            "dramatic ",
+            "eroul ",
+            "epic ",
+            "eul liric ",
+            "eul poetic ",
+            "final ","final.","finalul ","final deschis ","in final ",
+            "firul epic ",
+            "inceputul ",
+            "incipit ", "incipitul ",
+            "liric ",
+            "motivul ",
+            "momentele subiectului ",
+            "expozitiunea ","intriga ","desfasurarea actiunii","punctul culminant ","deznodamantul",
+            "in prima parte ","prima parte ","a doua parte ","a treia parte ","ultima parte ",
+            "nucleul I ","nucleul II ","nucleul III ",
+            "personajul ",
+            "personajul principal ",
+            "planuri narative ",
+            "primul volum ",
+            "sfarsitul ",
+            "stil ","stilul anticalofil ",
+            "simetria ","simetric ",
+            "scena ","scenele reprezentative ",
+            "titlul ","semnificatia titlului ",
+            "tema ",
+            "Volumul I ",
+            "Volumul II ",
+            "Volumul III "
+]});
+
+document.getElementById("poweredBy").innerHTML = "Powered by<br>DesignRoom.ro";
+
+
